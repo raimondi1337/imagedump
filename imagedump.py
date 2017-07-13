c5ac2a80b1b62f682dc9f0c7c3a52a0168c129d1
Dustin Raimondi <raimondi1337@gmail.com>
2017-07-13 15:08:00 -0700

    added image canvas and resizability

--- imagedump.py
+++ imagedump.py
@@ -4,16 +4,41 @@ import tkinter as tk
 from tkinter import filedialog
 
 class ImageDump():
+
 	# initilize program
 	def __init__(self):
+
 		# Data
 		self.root = tk.Tk()
 		self.source_directory = tk.StringVar()
 
-		# Build UI
+		# Build image canvas
+		self.canvas = tk.Canvas(self.root,bg='grey',highlightthickness=0)
+		self.original = Image.open('Drawing.png').convert("RGB")
+		self.image = ImageTk.PhotoImage(self.original)
+		self.canvas.create_image(0,0,image=self.image,anchor='nw',tags="IMG")
+		self.canvas.pack(fill="both", expand=True)
+		self.canvas.bind('<Configure>', self.resize)
+
+		# Build directory picker
 		self.browseLabel  = tk.Label( self.root,text='Base Directory:').pack(side='left')
-		self.browseEntry  = tk.Entry( self.root, textvariable=self.source_directory).pack(side='left')
+		self.browseEntry  = tk.Entry( self.root, textvariable=self.source_directory).pack(side='left',fill='x',expand=True)
 		self.browseButton = tk.Button(self.root, text='Browse', command=self.browse).pack(side='left')
+
+	# scale images to fit canvas
+	def resize(self,event):
+
+		# maths to find proper image size while maintaining aspect ratio
+		w,h = event.width,event.height
+		ow,oh = self.original.size
+		ratio = min((w/ow),(h/oh))
+		fitSize = (int(ow*ratio),int(oh*ratio))
+
+		# remove and replace resized image
+		self.image = self.original.resize(fitSize,Image.BILINEAR)
+		self.image = ImageTk.PhotoImage(self.image)
+		self.canvas.delete("IMG")
+		self.canvas.create_image(w/2,h/2,image=self.image,tags="IMG")
 
 	# get directory from user
 	def browse(self):
